441b15ef4f575a7f35ac315e3c36e44716f33f84
AdelinoJnr <adelinojunior96@gmail.com>
2021-06-29 18:27:58 -0300

    chore: initial project :rocket:

---
+++ src/components/shared/input/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { bool, string } from 'prop-types';
+
+function Input({ name, type, dataTestid, placeholder, ariaLabel, required }) {
+  return (
+    <input
+      name={ name }
+      type={ type }
+      data-testid={ dataTestid }
+      placeholder={ placeholder }
+      aria-label={ ariaLabel }
+      required={ required }
+    />
+  );
+}
+
+Input.propTypes = {
+  name: string.isRequired,
+  type: string.isRequired,
+  dataTestid: string,
+  placeholder: string.isRequired,
+  ariaLabel: string.isRequired,
+  required: bool.isRequired,
+};
+
+Input.defaultProps = {
+  dataTestid: '',
+};
+
+export default Input;
