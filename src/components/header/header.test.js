d150bb39b3aa3f3b8192041faa86cef15fdca506
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-06-30 18:09:59 -0300

    test: requisitos 10,11,12

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,11 +1,26 @@
 import renderPath from '../../test/functions';
 
+const searchButton = 'search-top-btn';
+
 describe('Test para o Header', () => {
-  it('', () => {
+  it('testa os botoes e redirecionamento para pagina de perfil', () => {
+    const { getByTestId } = renderPath('/comidas');
+    expect(getByTestId('profile-top-btn')).toBeInTheDocument();
+    expect(getByTestId('page-title')).toBeInTheDocument();
+    expect(getByTestId(searchButton)).toBeInTheDocument();
+    const perfil = getByTestId('profile-icon"');
+    userEvent.click(perfil);
+    expect(getByTestId('perfil')).toBeInTheDocument();
+  });
+
+  it('testa os botoes e serch bar aparecendo ao clicar', () => {
     const { getByTestId } = renderPath('/comidas');
 
     expect(getByTestId('profile-top-btn')).toBeInTheDocument();
     expect(getByTestId('page-title')).toBeInTheDocument();
-    expect(getByTestId('search-top-btn')).toBeInTheDocument();
+    expect(getByTestId(searchButton)).toBeInTheDocument();
+    const searchBar = getByTestId(searchButton);
+    userEvent.click(searchBar);
+    expect(getByTestId('searchBar')).toBeInTheDocument();
   });
 });
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SearchBar() {
   return (
     <div>
-      <span>searchBar</span>
+      <span data-testid="searchBar">searchBar</span>
     </div>
   );
 }
