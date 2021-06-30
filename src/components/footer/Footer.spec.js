fb8cf14fa155b2b6f680471e18abc76c69fa74b6
Willian Mendes <willianmendesbr@gmail.com>
2021-06-30 17:33:16 -0300

    test: must contain 3 links

--- src/components/footer/Footer.spec.js
+++ src/components/footer/Footer.spec.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './Footer';
 
 describe('footer', () => {
-  it('footer must exist', () => {
-    render(<Footer />);
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>,
+  );
 
+  it('footer must exist', () => {
     const footer = screen.getByTestId('footer');
     expect(footer);
+  });
+
+  it('must contain 3 links', () => {
+    const links = screen.getAllByRole('link');
+
+    const linkFoods = links.find((link) => link.getAttribute('href') === '/comidas');
+    const linkDrinks = links.find((link) => link.getAttribute('href') === '/bebidas');
+    const linkExplorer = links.find((link) => link.getAttribute('href') === '/explorar');
+
+    expect(linkFoods).toBeInTheDocument();
+    expect(linkDrinks).toBeInTheDocument();
+    expect(linkExplorer).toBeInTheDocument();
   });
 });
