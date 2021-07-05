b0a40b07cc89f6ef7b4210b0f887649fdf23ffb5
AdelinoJnr <adelinojunior96@gmail.com>
2021-07-05 17:50:06 -0300

    fix: done req 62

--- src/components/CardRecipeFavorite/index.jsx
+++ src/components/CardRecipeFavorite/index.jsx
@@ -1,24 +1,10 @@
 import { number, shape, string } from 'prop-types';
 import React from 'react';
-// import { getRecipeById } from '../../service/recipesApi';
-// import Button from '../shared/button';
 import shareIcon from '../../images/shareIcon.svg';
 import blackHeartIcon from '../../images/blackHeartIcon.svg';
 
 function CardRecipeFavorite({ index, recipe }) {
-  // const [recipeFavorite, setRecipeFavorite] = useState({});
   const { image, name, category, area, alcoholicOrNot, type } = recipe;
-
-  // useEffect(() => {
-  //   async function fetchApi() {
-  //     const data = await getRecipeById(id);
-  //     setRecipeFavorite(data);
-  //   }
-  //   fetchApi();
-  // });
-
-  // const { strTags } = recipeFavorite || { strTags: '' };
-  // console.log(strTags);
 
   function renderDrink() {
     return (
@@ -30,13 +16,21 @@ function CardRecipeFavorite({ index, recipe }) {
           alt={ name }
         />
         <p data-testid={ `${index}-horizontal-name` }>{ name }</p>
-        <p data-testid={ `${index}-horizontal-alcoholic` }>{ alcoholicOrNot }</p>
-        <p data-testid={ `${index}-horizontal-share-btn` } type="button">
-          <img src={ shareIcon } alt="Compartilhar" />
-        </p>
-        <p data-testid={ `${index}-horizontal-favorite-btn` } type="button">
-          <img src={ blackHeartIcon } alt="Favorito" />
-        </p>
+        <p data-testid={ `${index}-horizontal-top-text` }>{ alcoholicOrNot }</p>
+        <button type="button">
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt="Compartilhar"
+          />
+        </button>
+        <button type="button">
+          <img
+            data-testid={ `${index}-horizontal-favorite-btn` }
+            src={ blackHeartIcon }
+            alt="Favorito"
+          />
+        </button>
       </section>
     );
   }
@@ -52,12 +46,20 @@ function CardRecipeFavorite({ index, recipe }) {
         />
         <p data-testid={ `${index}-horizontal-name` }>{ name }</p>
         <p data-testid={ `${index}-horizontal-top-text` }>{ `${area} - ${category}` }</p>
-        <p data-testid={ `${index}-horizontal-share-btn` } type="button">
-          <img src={ shareIcon } alt="Compartilhar" />
-        </p>
-        <p data-testid={ `${index}-horizontal-favorite-btn` } type="button">
-          <img src={ blackHeartIcon } alt="Favorito" />
-        </p>
+        <button type="button">
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt="Compartilhar"
+          />
+        </button>
+        <button type="button">
+          <img
+            data-testid={ `${index}-horizontal-favorite-btn` }
+            src={ blackHeartIcon }
+            alt="Favorito"
+          />
+        </button>
       </section>
     );
   }
