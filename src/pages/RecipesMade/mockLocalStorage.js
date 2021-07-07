4b55ca68b7210b6f1dcab6fd66f05c41184752a9
Luciano Amâncio dos Santos <madruga665@pop-os.localdomain>
2021-07-07 00:56:32 -0300

    feat: recipes made

--- src/pages/RecipesMade/mockLocalStorage.js
+++ src/pages/RecipesMade/mockLocalStorage.js
@@ -23,16 +23,4 @@ const recipes = [
   },
 ];
 
-// [{
-//   id: id-da-receita,
-//   type: comida-ou-bebida,
-//   area: area-da-receita-ou-texto-vazio,
-//   category: categoria-da-receita-ou-texto-vazio,
-//   alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
-//   name: nome-da-receita,
-//   image: imagem-da-receita,
-//   doneDate: quando-a-receita-foi-concluida,
-//   tags: array-de-tags-da-receita-ou-array-vazio
-// }]
-
 export default recipes;
