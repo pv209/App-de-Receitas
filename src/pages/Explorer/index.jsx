c4c633e8e6c3f3587d5d67381ff95bc8847a58ec
AdelinoJnr <adelinojunior96@gmail.com>
2021-07-07 13:12:07 -0300

    feat: button explore drink and food

--- src/pages/Explorer/index.jsx
+++ src/pages/Explorer/index.jsx
@@ -4,8 +4,8 @@ import Footer from '../../components/footer';
 function Explorer() {
   return (
     <>
-      <button type="button" data-testid="explore-food">Comidas</button>
-      <button type="button" data-testid="explore-drinks">Bedidas</button>
+      <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
       <Footer />
     </>
   );
