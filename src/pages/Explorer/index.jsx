047f3190ee424d97e4fcf4c8595feec1f832735f
AdelinoJnr <adelinojunior96@gmail.com>
2021-07-07 13:15:07 -0300

    feat: redirect route pages

--- src/pages/Explorer/index.jsx
+++ src/pages/Explorer/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/footer';
 
 function Explorer() {
   return (
     <>
-      <button type="button" data-testid="explore-food">Explorar Comidas</button>
-      <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </>
   );
